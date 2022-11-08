1ae5f28f06293efa502eba7426caad8d5fc16386
Daniel He <tcdanielh@berkeley.edu>
2022-11-08 15:41:09 -0800

    Try fix UI

--- Assets/TutoriVR/Scripts/TiltbrushAppInfo.cs
+++ Assets/TutoriVR/Scripts/TiltbrushAppInfo.cs
@@ -40,8 +40,16 @@ public class TiltbrushAppInfo : MonoBehaviour, IAppInfo
     // Update is called once per frame
     void Update()
     {
-        if (rightController == null) rightController = GameObject.Find("Controller (brush)").transform;
-        if (leftController == null) leftController = GameObject.Find("Controller (wand)").transform;
+        if (rightController == null &&
+            GameObject.Find("UnityXRUninitializedControls(Clone)") == null)
+        {
+            rightController = GameObject.Find("Controller (brush)").transform;
+        }
+        if (leftController == null &&
+            GameObject.Find("UnityXRUninitializedControls(Clone)") == null)
+        {
+            leftController = GameObject.Find("Controller (wand)").transform;
+        }
         if (head == null && GameObject.Find("(RenderWrapper Camera)") != null) head = GameObject.Find("(RenderWrapper Camera)").transform;
         Debug.DrawRay(rightController.position, rightController.forward, Color.red);
         // bool rtVal = triggerAction.GetAxis(SteamVR_Input_Sources.RightHand) > 0.99f;
